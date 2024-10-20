d90e29e0777dc0337d9a470ac8fe8baeba79a2dc
unknown <nshienarocell@gmail.com>
2024-10-20 08:09:58 +0800

    initial commit

---
+++ objectActivity.js
@@ -0,0 +1,13 @@
+let car = {
+    type:"Sport Car",
+    model:"Aventador",
+    color:"Yellow"
+};
+
+console.log(typeof car);
+
+car.type = "lamborghini";
+console.log(car);
+
+car.wheels = 4;
+console.log (car);
